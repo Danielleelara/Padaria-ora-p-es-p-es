fa1cf39d33e7fe6291011909beb09166206c8714
Danielle <dany.sa1@hotmail.com>
2021-08-27 20:02:32 -0300

    feat: remover produto

--- adicionar_produto.js
+++ adicionar_produto.js
@@ -1,46 +1,50 @@
-var form = document.querySelector("form");
+(function(){
+  'use scrict';
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    var produto = obtemProdutoDoFormulario(event.target);
-    adicionaProdutoNaTabela(produto);
-    form.reset();
-});
+  const form = document.querySelector("form");
 
-function adicionaProdutoNaTabela(produto) {
-  var produtoTr = montaTr(produto);
-  var tabela = document.querySelector("#tabela-produtos");
-  tabela.appendChild(produtoTr);
-}
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      let produto = obtemProdutoDoFormulario(event.target);
+      adicionaProdutoNaTabela(produto);
+      form.reset();
+  });
 
-function obtemProdutoDoFormulario(form) {
- var produto = {
-    produto: form.produto.value,
-    quantidade: form.quantidade.value,
-    valor: form.valor.value,
-    total: form.total.value,
+  function adicionaProdutoNaTabela(produto) {
+    let produtoTr = montaTr(produto);
+    let tabela = document.querySelector("#tabela-produtos");
+    tabela.appendChild(produtoTr);
+  }
+
+  function obtemProdutoDoFormulario(form) {
+  let produto = {
+      produto: form.produto.value,
+      quantidade: form.quantidade.value,
+      valor: form.valor.value,
+      total: form.total.value,
+      
+    };
+
+    return produto;
+  }
+
+  function montaTr(produto) {
+    let produtoTr = document.createElement("tr");
+    produtoTr.classList.add("produto");
+
+    produtoTr.appendChild(montaTd(produto.produto, "produto"));
+    produtoTr.appendChild(montaTd(produto.quantidade, "quantidade"));
+    produtoTr.appendChild(montaTd(produto.valor, "valor"));
+    produtoTr.appendChild(montaTd(produto.total, "total"));
     
-  };
 
-  return produto;
-}
+    return produtoTr;
+  }
 
-function montaTr(produto) {
-  var produtoTr = document.createElement("tr");
-  produtoTr.classList.add("produto");
-
-  produtoTr.appendChild(montaTd(produto.produto, "produto"));
-  produtoTr.appendChild(montaTd(produto.quantidade, "quantidade"));
-  produtoTr.appendChild(montaTd(produto.valor, "valor"));
-  produtoTr.appendChild(montaTd(produto.total, "total"));
-  
-
-  return produtoTr;
-}
-
-function montaTd(dado, classe) {
-  var td = document.createElement("td");
-  td.textContent = dado;
-  td.classList.add(classe);
-  return td;
-}
+  function montaTd(dado, classe) {
+    let td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+  }
+})();
---
+++ remover_produto.js
@@ -0,0 +1,10 @@
+(function(){
+        'use strict'
+    const form = document.querySelector("form");
+
+    form.addEventListener('click', function(event) {
+       return this.remove.event
+        
+    });
+
+})();
